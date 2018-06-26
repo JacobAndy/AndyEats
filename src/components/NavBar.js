e6839eca652556d2716d9887b15ee2031d3382a9
Jacob Anderson <odistiinct@gmail.com>
2018-06-26 12:54:38 -0500

    navbar transition

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,23 @@ import React, { Component } from "react";
 import "../Sass/output.css";
 
 export default class Navbar extends Component {
-  state = {};
+  state = {
+    burgerToggle: false
+  };
   render() {
+    let { burgerToggle } = this.state;
     return (
-      <nav className="nav">
-        <section className="nav__sections">
+      <nav
+        className="nav"
+        onClick={() => this.setState({ burgerToggle: !burgerToggle })}
+      >
+        <section
+          className={
+            burgerToggle
+              ? "nav__sections nav-sections-visible"
+              : "nav__sections nav-sections-hidden"
+          }
+        >
           <h5>test</h5>
           <h5>test</h5>
           <h5>test</h5>
