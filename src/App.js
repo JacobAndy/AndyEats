5805acaafb686febb721e9b4c59a4a26a87e8f13
Aaron Blackshear <theshiftybapple@gmail.com>
2018-06-28 09:55:34 -0500

    I want chicken tendies

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import routes from "./routes";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import "./Sass/output.css";
 
 class App extends Component {
   render() {
     return (
       <div className="main-app-container">
-        {/* <NavBar /> */}
+        <NavBar />
         {routes}
         <Footer />
       </div>
