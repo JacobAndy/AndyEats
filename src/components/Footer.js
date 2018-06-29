32c5ca7520d416356dbeb3978f59454a5d8c498b
Aaron Blackshear <theshiftybapple@gmail.com>
2018-06-29 14:50:06 -0500

    Changed Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../Sass/output.css";
 
 export default function Footer() {
   return (
-    <main className="footer">
+    <main className="footer" style={{ color: "white" }}>
       <h6 className="footer__text">
         © Copyright 2019 Andy Eats Designed and Developed by Jacob Anderson and
         Aaron Blackshear
